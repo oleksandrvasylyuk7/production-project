bba33b887dfc46b57c628ebe267338ef59afeca8
Oleksandr Vasylyuk <oleksandr.vasylyuk@sebn.com>
2023-06-15 14:18:17 +0300

    Lesson 5

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BuildOptions} from './types/config';
 
 export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -7,6 +8,10 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
     new HtmlWebpackPlugin({
       template: paths.html
     }),
-    new webpack.ProgressPlugin()
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css'
+    })
   ];
 }
---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import {Counter} from './Counter';
+import '../index.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+      Counter
+      <Counter />
+    </div>
+  )
+}
+
+export default App;
