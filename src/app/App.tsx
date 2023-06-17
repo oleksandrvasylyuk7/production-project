b08232bb3c7de2a590550e4174987b4dfe11d84c
Oleksandr Vasylyuk <oleksandr.vasylyuk@sebn.com>
2023-06-17 14:17:24 +0300

    Lesson 14

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import {useTheme} from 'app/providers/ThemeProvider/lib/useTheme';
 import {classNames} from 'shared/lib/classNames/classNames';
 import {Navbar} from 'widgets/Navbar';
+import {Sidebar} from 'widgets/Sidebar';
 import {AppRouter} from 'app/providers/router';
 import 'app/styles/index.scss';
 
@@ -9,7 +10,10 @@ const App = () => {
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className='content-page'>
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   )
 }
