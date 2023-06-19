53274370ab29c50758fb2910f1fab5fc3d9a88b8
Oleksandr Vasylyuk <oleksandr.vasylyuk@sebn.com>
2023-06-19 15:08:07 +0300

    Lesson 21

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,24 +1,25 @@
 import { Route, Routes } from 'react-router-dom'
 import { Suspense } from 'react'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader'
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         {Object.values(routeConfig).map(({ path, element }) => (
           <Route
             key={path}
             path={path}
             element={
-              <div className='page-wrapper'>
-                {element}
-              </div>
+              <Suspense fallback={<PageLoader />}>
+                <div className='page-wrapper'>
+                  {element}
+                </div>
+              </Suspense>
             }
           />
         ))}
       </Routes>
-    </Suspense>
   )
 }
 
